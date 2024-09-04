7ca33a3a9a4b7ff5fedc4c70a903bee305028066
Rami James <ramijames@gmail.com>
2024-09-04 12:52:57 +0300

    For writers commission table is done

--- pages/index.vue
+++ pages/index.vue
@@ -24,16 +24,20 @@
   </main>
   <div id="Personas">
     <div class="container">
+      
       <span class="marker team">
         <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M18 19C18.5523 19 19.0016 18.5521 18.975 18.0004C18.9599 17.6868 18.9357 17.3593 18.8969 17.1156C18.8125 16.6047 18.1047 15.9297 15.0953 14.8234C12.1328 13.7359 12.3156 14.2656 12.3156 12.2641C12.3156 10.9656 12.9766 11.7203 13.3984 9.25469C13.5625 8.28438 13.6937 8.93125 14.05 7.375C14.2375 6.55938 13.9234 6.49844 13.9609 6.10938C13.9984 5.72031 14.0359 5.37344 14.1063 4.57656C14.1906 3.59219 13.2766 1 10 1C6.72344 1 5.80938 3.59219 5.89844 4.58125C5.96875 5.37344 6.00625 5.725 6.04375 6.11406C6.08125 6.50313 5.76719 6.56406 5.95469 7.37969C6.31094 8.93125 6.44219 8.28437 6.60625 9.25937C7.02813 11.725 7.68906 10.9703 7.68906 12.2687C7.68906 14.275 7.87187 13.7453 4.90937 14.8281C1.9 15.9297 1.1875 16.6094 1.10781 17.1203C1.06725 17.3619 1.04194 17.6879 1.02616 18.0004C0.998311 18.552 1.44772 19 2 19H10H18Z" stroke="#8D00FB" stroke-opacity="0.9" stroke-width="2" stroke-linecap="round"/>
         </svg>
         For teams
       </span>
+      
       <h2>
         Efficiently manage your Technical Writing team
       </h2>
+      
       <p>Our software provides your team with a customizable, tailored alternative for project management of technical writing projects. You don’t have to use tools made for developers and marketers to get your work done on-time and within budget.</p>
+      
       <div class="illustration stakeholder">
         <div class="text bottom-left">
           <h3>Stakeholder visibility</h3>
@@ -45,12 +49,10 @@
         <div class="illustration-bubble">
           <h3>State management</h3>
           <p>As deliverables make their way through your workflow, they have discrete states that have clear meanings that map to your business processes.</p>
-
         </div>
         <div class="illustration-bubble">
           <h3>Smart templates</h3>
           <p>Teams that are just getting started can rely on our smart default templates to quickly set up their processes and hit the ground running.</p>
-
         </div>
       </div>
 
@@ -60,12 +62,69 @@
           <p>Already have an established process? Vewrite allows you to build custom worksflows that fit your existing pipeline.</p>
         </div>
       </div>
+      
+      
       <span class="marker writer">
         <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" clip-rule="evenodd" d="M1.2897 14.9077C1.10412 15.0949 1 15.3479 1 15.6116V18C1 18.5523 1.44772 19 2 19H4.38313C4.64992 19 4.90564 18.8934 5.09343 18.7039L15.0993 8.60709C15.4868 8.21606 15.4854 7.58536 15.0961 7.19609L12.7103 4.81031C12.3185 4.41854 11.6829 4.41998 11.2929 4.81352L1.2897 14.9077ZM18.7 5C19.1 4.6 19.1 4 18.7 3.6L16.4 1.3C16 0.9 15.4 0.9 15 1.3L13.9071 2.39289C13.5166 2.78342 13.5166 3.41658 13.9071 3.80711L16.2525 6.15252C16.6585 6.55846 17.3221 6.54004 17.7049 6.11221L18.7 5Z" stroke="#68834A" stroke-width="2" stroke-linecap="round"/>
         </svg>
         For writers
       </span>
+
+      <h2>Find well-paid work as a Technical Writer within our integrated Talent Marketplace</h2>
+      <p>Vewrite tightly integrates a marketplace for technical writers into its project management suite. Writers who want to work on awesome projects can now do so without losing a large percentage of their earnings to marketplace fees.</p>
+
+      <table class="commission-rates">
+        <thead>
+          <tr>
+            <th></th>
+            <th></th>
+            <th>Buyer cost</th>
+            <th>Writer cost</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="marketplace-fiverr">
+            <td></td>
+            <td><img src="/images/logo-fiverr.png" alt="Fiverr" /></td>
+            <td>
+              <span><strong>5.5% Service Fee</strong></span>
+              <span><small>+ $2.50 for transactions under $75</small></span>
+            </td>
+            <td>
+              <span><strong>20% Commission</strong></span>
+            </td>
+            <td></td>
+          </tr>
+          <tr class="marketplace-upwork">
+            <td></td>
+            <td><img src="/images/logo-upwork.png" alt="Upwork" /></td>
+            <td>
+              <span><strong>$4.95</strong></span>
+              <span><small>Initial contract cost</small></span>
+            </td>
+            <td>
+              <span><strong>10% Commission</strong></span>
+            </td>
+            <td></td>
+          </tr>
+          <tr class="marketplace-vewrite">
+            <td></td>
+            <td><img src="/images/logo-vewrite.png" alt="Vewrite" /></td>
+            <td>
+              <span><strong>2.5% Service Fee</strong></span>
+            </td>
+            <td>
+              <span><strong>Free!</strong></span>
+            </td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+
+
+
     </div>
   </div>
 </template>
@@ -305,6 +364,7 @@ definePageMeta({
     &.writer {
       background: rgba($green, 0.15);
       color: $green-dark;
+      margin-top: $spacing-xxl;
     }
   }
 
@@ -356,6 +416,127 @@ definePageMeta({
     border-radius: $br-xl;
     width: 50%;
     background: rgba($purple, 0.05);
+  }
+}
+
+.commission-rates {
+  width: 100%;
+
+  thead {
+    tr {
+      th {
+        font-size: $font-size-xl;
+        font-weight: normal;
+        font-family: $font-family-secondary;
+        color: $green-dark;
+        text-align: left;
+        padding: $spacing-md $spacing-lg;
+        border: none;
+
+        &:nth-child(1) {
+          width: 5%;
+          padding: 0;
+
+          @media (max-width: 768px) {
+            display: none;
+          }
+        }
+
+        &:nth-child(2) {
+          width: 20%;
+          padding: 0;
+        }
+
+        &:nth-child(3) {
+          background-color: #EEF2E7;
+          border-radius: $br-xl 0 0 0;
+          width: 35%;
+        }
+
+        &:nth-child(4) {
+          background-color: #EEF2E7;
+          border-radius: 0 $br-xl 0 0;
+          width: 35%;
+        }
+
+        &:nth-child(5) {
+          width: 5%;
+          padding: 0;
+
+          @media (max-width: 768px) {
+            display: none;
+          }
+        }
+      }
+    }
+  }
+
+  tbody {
+    tr {
+      
+
+      td {
+        font-size: $font-size-md;
+        font-family: $font-family-condensed;
+        text-align: left;
+        padding: $spacing-lg;
+        border-top: 2px solid white;
+
+        span {
+          display: block;
+          font-size: $font-size-lg;
+
+          small {
+            font-size: $font-size-xs;
+          }
+        }
+
+        &:nth-child(1) {
+          width: 5%;
+          padding: 0;
+
+          @media (max-width: 768px) {
+            display: none;
+          }
+        }
+
+        &:nth-child(3) {
+          background-color: #EEF2E7;
+        }
+
+        &:nth-child(4) {
+          background-color: #EEF2E7;
+        }
+
+        &:nth-child(5) {
+          width: 5%;
+          padding: 0;
+
+          @media (max-width: 768px) {
+            display: none;
+          }
+        }
+      }
+    }
+
+    tr.marketplace-vewrite {
+      
+      td {
+        background: $purple;
+        color: $white;
+        padding: $spacing-lg;
+        border-top: none;
+
+        &:nth-child(1) {
+          border-radius: $br-xl 0 0 $br-xl;
+        }
+
+        &:nth-child(5) {
+          border-radius: 0 $br-xl $br-xl 0;
+        }
+      }
+      
+    }
   }
 }
 
