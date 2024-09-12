eb0561413682ebe271c1a9686bcfba1651e9b01d
Rami James <ramijames@gmail.com>
2024-09-12 09:12:17 +0300

    Trying to get hotjar to verify

--- app.vue
+++ app.vue
@@ -8,7 +8,6 @@
 
 useHead({
   script: [{ children: "
-<script>
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:5129596,hjsv:6};
@@ -17,7 +16,7 @@ useHead({
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>" }],
+" }],
 });
 
 </script>
