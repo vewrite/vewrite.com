ceb901bc3d262b4c6d39aeb8efa620a35fb32ad2
Rami James <ramijames@gmail.com>
2025-01-01 02:52:34 -0500

    Mobile menu

--- components/v2/Header.vue
+++ components/v2/Header.vue
@@ -1,9 +1,16 @@
 <template>
   <nav>
     <section :class="['mobile', menu]">
-      <button class="mobile-toggle" @click="toggleMenu">
-        Menu
-      </button>
+      <section class="toggle-menu-logo">
+        <button class="mobile-toggle large" @click="toggleMenu">
+          <svg width="18" height="13" viewBox="0 0 18 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 1.5H17" stroke="black" stroke-width="2" stroke-linecap="round"/>
+            <path d="M1 6.5H17" stroke="black" stroke-width="2" stroke-linecap="round"/>
+            <path d="M1 11.5H17" stroke="black" stroke-width="2" stroke-linecap="round"/>
+          </svg>
+        </button>
+        <v2Logo />
+      </section>
     </section>
     <section class="desktop"></section>
   </nav>
@@ -18,3 +25,30 @@ const toggleMenu = function(){
 }
 
 </script>
+
+<style lang="scss" scoped>
+
+@use 'assets/variables' as *;
+
+nav {
+
+  .mobile {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+
+    .toggle-menu-logo {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+      align-items: center;
+      width: 100%;
+      padding: $spacing-sm;
+      gap: $spacing-sm;
+    }
+  }
+}
+
+</style>
